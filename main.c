2bd6b994548f8547f0225239e74e3547e4e0f274
Leeo-Devops <leonardo65921@gmail.com>
2025-02-23 19:01:38 +0000

    criei um cadastro de cartas

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (){
+    char Estado;
+    (char[a01]) Codigo da carta;
+    nome da cidade, (char[]);
+    int população;
+    float Pib;
+    int Numero de pontos turistico;
+
+    printf("Digite o nome do Estado: \n");
+    scanf("%d", &Estado);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &Codigo da carta);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nome da cidade);
+
+    printf("Digite a população: \n");
+    scanf("%f" &Pib);
+
+    printf("Ponto turistico: \n");
+    scanf("%d", &turistico);
+
+    printf("Estado: %s - Codigo da carta %d", Codigo da carta, nome da cidade );
+    printf("população: %d - Pib %f", Estado);
+    
+    return 0;
+}
